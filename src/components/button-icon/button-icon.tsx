71bf8724236f884474be154361cbe23c9b2aae8d
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2024-10-29 01:38:33 +0300

    Добавляет событие

--- src/components/button-icon/button-icon.tsx
+++ src/components/button-icon/button-icon.tsx
@@ -2,11 +2,12 @@ import './button-icon.scss';
 
 type Icon = {
     icon: string,
+    setModalActive: (arg: boolean) => void;
 }
-const ButtonIcon = ({ icon }: Icon) => {
+const ButtonIcon = ({ icon, setModalActive }: Icon) => {
     
     return (
-        <div className='button'>
+        <div className='button' onClick={setModalActive ? () => setModalActive(false) : () => false }>
             <img src={icon} />
         </div>
     )
